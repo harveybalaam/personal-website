80cf8abbcdd0755bafcc7218c056415a297b34d5
Harvey Balaam <harvey.balaam@pimberly.com>
2026-05-24 18:13:50 +0100

    feat: add title to links page

--- src/pages/Links/index.tsx
+++ src/pages/Links/index.tsx
@@ -1,28 +1,37 @@
 import { faGithub, faSquareLinkedin } from "@fortawesome/free-brands-svg-icons";
+import linksTitle from "../../assets/images/links-title.svg";
 import LinkCard from "./LinkCard";
 import "./Links.css";
 
 export default function Links() {
   return (
-    <div className="link-container centered">
-      <LinkCard
-        description="Connect with me"
-        icon={faSquareLinkedin}
-        linksTo="https://www.linkedin.com/in/harveybalaam/"
-        title="LinkedIn"
-      />
-      <LinkCard
-        icon={faGithub}
-        description="Take a look at my personal projects (including this site!)"
-        linksTo="https://github.com/harveybalaam"
-        title="GitHub"
-      />
-      <LinkCard
-        icon={faGithub}
-        description="Here you can find projects I created during my degree"
-        linksTo="https://github.com/skiing-gecko"
-        title="GitHub (University Profile)"
-      />
-    </div>
+    <>
+      <div className="links-title centered">
+        <img
+          src={linksTitle}
+          alt="ASCII art page title stating 'More from Me'"
+        />
+      </div>
+      <div className="links-container centered">
+        <LinkCard
+          description="Connect with me"
+          icon={faSquareLinkedin}
+          linksTo="https://www.linkedin.com/in/harveybalaam/"
+          title="LinkedIn"
+        />
+        <LinkCard
+          icon={faGithub}
+          description="Take a look at my personal projects (including this site!)"
+          linksTo="https://github.com/harveybalaam"
+          title="GitHub"
+        />
+        <LinkCard
+          icon={faGithub}
+          description="Here you can find projects I created during my degree"
+          linksTo="https://github.com/skiing-gecko"
+          title="GitHub (University Profile)"
+        />
+      </div>
+    </>
   );
 }
