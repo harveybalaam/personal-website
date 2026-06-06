9b4c923179208c3cb0d7c684db32ddfe98dbd313
Harvey Balaam <harvey.balaam@pimberly.com>
2026-06-06 15:41:02 +0100

    feat: create project component

--- src/pages/PersonalProjects/Project/index.tsx
+++ src/pages/PersonalProjects/Project/index.tsx
@@ -1,5 +1,76 @@
+import {
+  faChevronRight,
+  faChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { type Project as ProjectProps } from "..";
 import "./Project.css";
+import Tag from "../../../components/Tag";
 
-export default function Project() {
-  return <></>;
+export default function Project({
+  concepts,
+  description,
+  isFeatured,
+  keyTakeaways,
+  link,
+  tags,
+  technicalDetails,
+  title,
+}: ProjectProps) {
+  const [isCollapsed, setIsCollapsed] = useState(!isFeatured);
+
+  return (
+    <div className="container project">
+      <button
+        className="project-title"
+        onClick={() => setIsCollapsed(!isCollapsed)}
+      >
+        <span className="icon-container icon-container-sm">
+          <FontAwesomeIcon
+            icon={isCollapsed ? faChevronRight : faChevronDown}
+          />
+        </span>
+        <h4>{title}</h4>
+      </button>
+      {!isCollapsed && (
+        <div className="project-body">
+          <div className="tags">
+            {tags.map((tag) => (
+              <Tag
+                key={`${tag.text}-${tag.colour}`}
+                colour={tag.colour}
+                text={tag.text}
+              />
+            ))}
+          </div>
+          <div>
+            <h4>Key Concepts</h4>
+            <span className="list-details">{concepts.join(" | ")}</span>
+          </div>
+          <div>
+            <h4>Technologies Used</h4>
+            <span className="list-details">{technicalDetails.join(" | ")}</span>
+          </div>
+          <div>
+            <h4>Project Overview</h4>
+            {description}
+          </div>
+          <div>
+            <h4>Key Takeaways</h4>
+            {keyTakeaways}
+          </div>
+          {link ? (
+            <a href={link} target="_blank" className="project-link">
+              View Source
+            </a>
+          ) : (
+            <span className="italic-sm">
+              Note: this project's source code is not currently public
+            </span>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
--- src/pages/PersonalProjects/index.tsx
+++ src/pages/PersonalProjects/index.tsx
@@ -1,5 +1,67 @@
+import Project from "./Project";
 import "./PersonalProjects.css";
 
+interface Tag {
+  colour?: "orange" | "red" | "pink" | "purple" | "blue" | "green" | "yellow";
+  text: string;
+}
+
+export interface Project {
+  concepts: string[];
+  description: string;
+  id: number;
+  isFeatured: boolean;
+  keyTakeaways: string;
+  link?: string;
+  tags: Tag[];
+  technicalDetails: string[];
+  title: string;
+}
+
+const projects: Project[] = [];
+
 export default function PersonalProjects() {
-  return <></>;
+  const featuredProjects = projects.filter((project) => project.isFeatured);
+  const hasFeaturedProjects = featuredProjects.length > 0;
+
+  return (
+    <div className="projects">
+      {hasFeaturedProjects && (
+        <>
+          <h3 className="project-section-header">{`Featured Project${featuredProjects.length > 1 ? "s" : ""}`}</h3>
+          {featuredProjects.map((project) => (
+            <Project
+              key={project.id}
+              concepts={project.concepts}
+              description={project.description}
+              id={project.id}
+              isFeatured={project.isFeatured}
+              keyTakeaways={project.keyTakeaways}
+              link={project.link}
+              tags={project.tags}
+              technicalDetails={project.technicalDetails}
+              title={project.title}
+            />
+          ))}
+        </>
+      )}
+      <h3 className="project-section-header">All Projects</h3>
+      {projects
+        .filter((project) => !project.isFeatured)
+        .map((project) => (
+          <Project
+            key={project.id}
+            concepts={project.concepts}
+            description={project.description}
+            id={project.id}
+            isFeatured={project.isFeatured}
+            keyTakeaways={project.keyTakeaways}
+            link={project.link}
+            tags={project.tags}
+            technicalDetails={project.technicalDetails}
+            title={project.title}
+          />
+        ))}
+    </div>
+  );
 }
