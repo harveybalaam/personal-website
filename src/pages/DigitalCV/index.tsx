50f8eba19ab19d623637df602cd0dd4f0a1873aa
Harvey Balaam <harvey.balaam@pimberly.com>
2026-07-26 22:56:53 +0100

    feat: create cv section containers

--- src/pages/DigitalCV/index.tsx
+++ src/pages/DigitalCV/index.tsx
@@ -1,3 +1,21 @@
+import Container from "../../components/Container";
+import "./DigitalCV.css";
+
 export default function DigitalCV() {
-  return <></>;
+  return (
+    <div className="cv-content-wrapper">
+      <div className="cv-content">
+        <div className="cv-content-row">
+          <Container title="Experience"></Container>
+        </div>
+        <div className="cv-content-row">
+          <Container title="Education"></Container>
+          <Container title="Skills & Certifications"></Container>
+        </div>
+        <div className="cv-content-row">
+          <Container title="Volunteer Roles & Other Experience"></Container>
+        </div>
+      </div>
+    </div>
+  );
 }
