e69f34d18fbb0bb55b71c1c3be11206d12d94595
Harvey Balaam <harvey.balaam@pimberly.com>
2026-07-28 22:15:21 +0100

    fix: update hello page content

--- src/pages/Hello/index.tsx
+++ src/pages/Hello/index.tsx
@@ -9,7 +9,27 @@ export default function Hello() {
         <img src={helloTitle} alt="ASCII art page title which reads 'Hello!'" />
       </div>
       <div className="hello-body centered">
-        <p>Welcome to my personal site! Here you can find information about me, any personal projects I've built (and am currently building), as well as my professional &amp; educational experience. I've also included some <Link className="text-link outline-blue" to="/links">&lt;links&gt;</Link> to other places where you can find out more, such as LinkedIn and GitHub.<br /><br />I've tried to recreate the vibe of a TUI application with this site. My main goal was to create something that is not just functional but also enjoyable to interact with, and I hope I have succeded in that! :)</p>
+        <p>
+          Welcome to my personal site! Here you can find general information
+          about me, the personal projects I&apos;ve built (and am currently
+          building), as well as my professional &amp; educational experience.
+          I&apos;ve also included some{" "}
+          <Link className="text-link outline-blue" to="/links">
+            &lt;links&gt;
+          </Link>{" "}
+          to other places where you can find out more, such as my LinkedIn and
+          GitHub profiles.
+          <br />
+          <br />
+          As you may have noticed, I&apos;ve tried to recreate the vibe of a TUI
+          application with this site. My aim is to create something that is not
+          just functional, but also enjoyable to interact with. While I&apos;m
+          not sure this site will ever be &quot;done&quot;, I hope you feel that
+          I am at least heading in that direction! :)
+          <br />
+          <br />
+        </p>
+        <p id="copyright">&copy; 2026 Harvey Balaam</p>
       </div>
     </>
   );
